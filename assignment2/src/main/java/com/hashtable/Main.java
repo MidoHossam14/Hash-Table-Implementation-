035038629aca330df7855c7244bf692fa96c5fe9
MidoHossam14 <mido14955@gmail.com>
2024-12-06 16:25:11 +0200

    Some Editing In GUI & More Proved results in Search & Update methods.

--- assignment2/src/main/java/com/hashtable/Main.java
+++ assignment2/src/main/java/com/hashtable/Main.java
@@ -49,174 +49,175 @@ public class Main {
         header.setBounds(0, 0, 854, 30);
         header.setLayout(null);
 
-        JPanel input = new JPanel();
-        input.setBackground(new Color(239, 240, 232));
-        input.setBounds(0, 30, 412, 450);
-        input.setLayout(null);
-
-        JPanel output = new JPanel();
-        output.setBackground(new Color(239, 240, 232));
-        output.setBounds(input.getWidth() + 1, 30, 441, 450);
-        output.setLayout(null);
-        JTextField message = createTextField(Color.BLACK, output.getBackground());
+        JPanel content = new JPanel();
+        content.setBackground(new Color(239, 240, 232));
+        content.setBounds(0, 30, 854, 450);
+        content.setLayout(null);
+        JTextField message = createTextField(Color.BLACK, content.getBackground());
         message.setEditable(false);
         message.setBorder(null);
 
         JButton insert = createButton("Insert", header.getBackground(), new Color(184, 218, 186));
-        insert.setBounds(0, 0, insert.getPreferredSize().width, 30);
+        insert.setBounds(50, 0, 100, 30);
         JButton remove = createButton("Remove", header.getBackground(), new Color(184, 218, 186));
-        remove.setBounds(insert.getWidth(), 0, remove.getPreferredSize().width, 30);
+        remove.setBounds(insert.getX()+insert.getWidth()+50, 0, 100, 30);
         JButton update = createButton("Update", header.getBackground(), new Color(184, 218, 186));
-        update.setBounds(remove.getX() + remove.getWidth(), 0, update.getPreferredSize().width, 30);
+        update.setBounds(remove.getX() + remove.getWidth()+50, 0, 100, 30);
         JButton search = createButton("Search", header.getBackground(), new Color(184, 218, 186));
-        search.setBounds(update.getX() + update.getWidth(), 0, search.getPreferredSize().width, 30);
+        search.setBounds(update.getX() + update.getWidth()+50, 0, 100, 30);
         JButton print = createButton("Print", header.getBackground(), new Color(184, 218, 186));
-        print.setBounds(search.getX() + search.getWidth(), 0, print.getPreferredSize().width, 30);
+        print.setBounds(search.getX() + search.getWidth()+50, 0, 100, 30);
         JButton exit = createButton("X", header.getBackground(), new Color(212, 62, 62));
         exit.setBounds(809, 0, 45, 30);
 
         JLabel name1 = createLabel("Name");
-        name1.setBounds(116, 90, 43, 27);
-        JTextField name2 = createTextField(Color.BLACK, output.getBackground());
-        name2.setBounds(206, 90, 150, 27);
+        name1.setBounds(353, 90, 43, 27);
+        JTextField name2 = createTextField(Color.BLACK, content.getBackground());
+        name2.setBounds(443, 90, 150, 27);
 
         JLabel phoneNumber1 = createLabel("Phone Number");
-        phoneNumber1.setBounds(81, 167, 113, 27);
-        JTextField phoneNumber2 = createTextField(Color.BLACK, output.getBackground());
-        phoneNumber2.setBounds(206, 167, 150, 27);
+        phoneNumber1.setBounds(318, 167, 113, 27);
+        JTextField phoneNumber2 = createTextField(Color.BLACK, content.getBackground());
+        phoneNumber2.setBounds(443, 167, 150, 27);
 
         insert.addActionListener(e -> {
-            input.removeAll();
-            output.removeAll();
-            output.repaint();
-            input.add(name1);
-            input.add(name2);
-            input.add(phoneNumber1);
-            input.add(phoneNumber2);
+            content.removeAll();
+            content.repaint();
+            content.add(name1);
+            name2.setText(null);
+            content.add(name2);
+            content.add(phoneNumber1);
+            phoneNumber2.setText(null);
+            content.add(phoneNumber2);
             JButton insertButton = createButton("Insert", header.getBackground(), new Color(184, 218, 186));
-            insertButton.setBounds(101, 300, 250, 37);
+            insertButton.setBounds(353, 250, 200, 37);
             insertButton.addActionListener(e1 -> {
                 if (!name2.getText().isEmpty() && !phoneNumber2.getText().isEmpty()) {
                     String name = name2.getText();
                     String phoneNumber = phoneNumber2.getText();
                     if (Table.insertEntry(name, phoneNumber)) {
                         message.setText(name + " is successfully Inserted");
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setForeground(Color.GREEN);
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     } else {
                         message.setText("Table is full");
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     }
                     name2.setText("");
                     phoneNumber2.setText("");
                 }
             });
-            input.add(insertButton);
-            input.repaint();
+            content.add(insertButton);
+            content.repaint();
         });
         header.add(insert);
 
         remove.addActionListener(e -> {
-            input.removeAll();
-            output.removeAll();
-            output.repaint();
-            input.add(name1);
-            input.add(name2);
+            content.removeAll();
+            content.repaint();
+            content.add(name1);
+            name2.setText(null);
+            content.add(name2);
             JButton removeButton = createButton("Remove", header.getBackground(), new Color(184, 218, 186));
-            removeButton.setBounds((input.getWidth() / 2) - 105, input.getHeight() * 2 / 3, 250,
-                    removeButton.getPreferredSize().height);
+            removeButton.setBounds(353, 250, 200, 37);
             removeButton.addActionListener(e1 -> {
                 if (!name2.getText().isEmpty()) {
-                    String name = name2.getText();
+                    String name = name2.getText().stripIndent();
                     if (Table.removeEntry(name)) {
                         message.setText(name + " is successfully Removed");
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     } else {
                         message.setText("Contact is not found");
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setForeground(Color.RED);
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     }
                     name2.setText("");
                 }
             });
-            input.add(removeButton);
-            input.repaint();
+            content.add(removeButton);
+            content.repaint();
         });
         header.add(remove);
 
         update.addActionListener(e -> {
-            input.removeAll();
-            output.removeAll();
-            output.repaint();
-            input.add(name1);
-            input.add(name2);
-            input.add(phoneNumber1);
-            input.add(phoneNumber2);
+            content.removeAll();
+            content.repaint();
+            content.add(name1);
+            name2.setText(null);
+            content.add(name2);
+            content.add(phoneNumber1);
+            phoneNumber2.setText(null);
+            content.add(phoneNumber2);
             JButton updateButton = createButton("Update", header.getBackground(), new Color(184, 218, 186));
-            updateButton.setBounds((input.getWidth() / 2) - 105, input.getHeight() * 2 / 3, 250,
-                    updateButton.getPreferredSize().height);
+            updateButton.setBounds(353, 250, 200, 37);
             updateButton.addActionListener(e1 -> {
                 if (!name2.getText().isEmpty() && !phoneNumber2.getText().isEmpty()) {
-                    String name = name2.getText();
+                    String name = name2.getText().stripIndent();
                     String phoneNumber = phoneNumber2.getText();
                     if (Table.updateEntry(name, phoneNumber)) {
                         message.setText(name + " updated");
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setForeground(Color.GREEN);
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     } else {
                         message.setText("Contact is not found");
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setForeground(Color.RED);
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     }
                     name2.setText("");
                     phoneNumber2.setText("");
                 }
             });
-            input.add(updateButton);
-            input.repaint();
+            content.add(updateButton);
+            content.repaint();
         });
         header.add(update);
 
         search.addActionListener(e -> {
-            input.removeAll();
-            output.removeAll();
-            output.repaint();
-            input.add(name1);
-            input.add(name2);
+            content.removeAll();
+            content.repaint();
+            content.add(name1);
+            name2.setText(null);
+            content.add(name2);
             JButton searchButton = createButton("Search", header.getBackground(), new Color(184, 218, 186));
-            searchButton.setBounds((input.getWidth() / 2) - 105, input.getHeight() * 2 / 3, 250,
-                    searchButton.getPreferredSize().height);
+            searchButton.setBounds(353, 250, 200, 37);
             searchButton.addActionListener(e1 -> {
                 if (!name2.getText().isEmpty()) {
-                    String name = name2.getText();
+                    String name = name2.getText().stripIndent();
                     if (Table.searchEntry(name) != null) {
                         message.setText(name + "’s phone number is " + Table.searchEntry(name));
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     } else {
                         message.setText("Contact is not found");
-                        message.setBounds(95, 27, 250, 27);
-                        output.add(message);
-                        output.repaint();
+                        message.setForeground(Color.RED);
+                        message.setBounds(318, 350, 250, 27);
+                        content.add(message);
+                        content.repaint();
                     }
                     name2.setText("");
                 }
             });
-            input.add(searchButton);
-            input.repaint();
+            content.add(searchButton);
+            content.repaint();
         });
         header.add(search);
 
         print.addActionListener(e -> {
+            name2.setText(null);
+            phoneNumber2.setText(null);
             frame.setVisible(false);
             Object[][] data = Table.getTableData();
             String[] columns = { "Key", "Index", "Value" };
@@ -270,8 +271,7 @@ public class Main {
         header.add(exit);
 
         frame.add(header);
-        frame.add(input);
-        frame.add(output);
+        frame.add(content);
         frame.setVisible(true);
     }
 
